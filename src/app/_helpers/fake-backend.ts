0d0f597e775c85c6bbcd358fe3e0e9eca35ed8bc
Attila7798 <attila101498gmail.com>
2023-01-24 19:38:21 +0100

    Finished fake-backend, added error & jwt interceptor, finished account-service

--- src/app/_helpers/fake-backend.ts
+++ src/app/_helpers/fake-backend.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
-import { delay, dematerialize, materialize, Observable, of, throwError } from 'rxjs';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, dematerialize, materialize } from 'rxjs/operators';
 
 // tömb a localStorage-ban a regisztrált felhasználók számára
 const userKey = 'users';
@@ -56,6 +57,44 @@ export class FakeBackendInterceptor implements HttpInterceptor {
             return ok();
         }
 
+        function getUsers() {
+            if (!isLoggedIn()) return unauthorized();
+            return ok(users.map(x => basicDetails(x)));
+        }
+
+        function getUserById() {
+            if (!isLoggedIn()) return unauthorized();
+
+            const user = users.find(x => x.id === idFromUrl());
+            return ok(basicDetails(user));
+        }
+
+        function updateUser() {
+            if (!isLoggedIn()) return unauthorized();
+
+            let params = body;
+            let user = users.find(x => x.id === idFromUrl());
+
+            // Csak akkor módosítjuk a jelszót, ha van benne érték
+            if (!params.password) {
+                delete params.password;
+            }
+
+            Object.assign(user, params);
+            localStorage.setItem(userKey, JSON.stringify(users));
+
+            return ok();
+        }
+
+        function deleteUser() {
+            if (!isLoggedIn()) return unauthorized();
+
+            users = users.filter(x => x.id !== idFromUrl());
+            localStorage.setItem(userKey, JSON.stringify(users));
+
+            return ok();
+        }
+
         function error(message: string) {
             return throwError(() => ({ error: { message } })).pipe(
                 materialize(), delay(500), dematerialize()
@@ -76,5 +115,27 @@ export class FakeBackendInterceptor implements HttpInterceptor {
             const { id, username, firstName, lastName } = user;
             return { id, username, firstName, lastName };
         }
+
+        function isLoggedIn() {
+            return headers.get('Authorization') === 'Bearer fake-jwt-token';
+        }
+
+        function unauthorized() {
+            return throwError(() => ({ status: 401, error: { message: 'Unauthorized' } }))
+                .pipe(materialize(), delay(500), dematerialize());
+        }
+
+        function idFromUrl() {
+            const urlParts = url.split('/'); // Ez lesz belőle -> ["users", "edit", "1"]
+            return parseInt(urlParts[urlParts.length - 1]);
+        }
     }
+}
+
+export const fakeBackendProvider = {
+    // Hamis backend-et használunk
+    // backend less development
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
 }
